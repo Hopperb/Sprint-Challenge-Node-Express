019ea3f24c73274d0c02603a231812ddd70fa5ac
Brandon Hopper <brandonhopper0@gmail.com>
2018-06-01 17:07:37 -0400

    -created endpoint for GET request and GET request by ID

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const db = require('./data/dbConfig');
 // database helpers
 const action = require('./data/helpers/actionModel');
 const project = require('./data/helpers/projectModel');
+const map = require('./data/helpers/mappers');
 
 // server code 
 const port = 5000;
@@ -14,8 +15,40 @@ const server = express();
 server.use(express.json());
 
 
+// GET REQUEST 
+// =============Project===================
+server.get('/api/project/', (req, res) => {
+    project
+    .get()
+    .then(data => res.json(data))
+    .catch(err => {res.json(err)})
+})
+// by ID
+server.get('/api/project/:id', (req, res) => {
+    const {id} = req.params
+    project
+    .get(id)
+    .then(data => res.json(data))
+    .catch(err => {res.json(err)})
+})
+// ==========================================
 
-
+// ==============Action=====================
+server.get('/api/action/', (req, res) => {
+    action
+    .get()
+    .then(data => res.json(data))
+    .catch(err => {res.json(err)})
+})
+// by ID
+server.get('/api/action/:id', (req, res) => {
+    const {id} = req.params
+    action
+    .get(id)
+    .then(data => res.json(data))
+    .catch(err => {res.json(err)})
+})
+// ==============================================
 
 
 
